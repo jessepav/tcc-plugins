67c10e7d98bbb21a9a4407a8d9e2dfc2aa8641a7
Jesse Pavel <jpavel@alum.mit.edu>
2022-01-27 15:41:07 -0500

    Add bare DLL plugin VS2019 project

---
+++ swizzle/swizzle-pch.h
@@ -0,0 +1,18 @@
+#ifndef SWIZZLE_PCH_H
+#define SWIZZLE_PCH_H
+
+#define UNICODE 1
+#define _UNICODE 1
+
+#define _ATL_ALLOW_CHAR_UNSIGNED 1
+
+#include <stdio.h>
+#include <tchar.h>
+
+#define WIN32_LEAN_AND_MEAN     // Exclude rarely-used stuff from Windows headers
+#include <windows.h>
+
+#include "PlugIn.h"
+#include "TakeCmd.h"
+
+#endif //SWIZZLE_PCH_H
---
+++ swizzle/swizzle.cpp
@@ -0,0 +1,22 @@
+// TCC-Swizzle.cpp : Defines the exported functions for the DLL.
+//
+
+#include "swizzle-pch.h"
+
+#define PLUGIN_API __declspec(dllexport)
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved
+)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
