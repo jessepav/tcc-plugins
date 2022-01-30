997e0769235df09a10befc2d22233999fa2f52c0
Jesse Pavel <jpavel@alum.mit.edu>
2022-01-30 14:37:15 -0500

    Parse arguments to f_swizzle

--- swizzle/src/pch.h
+++ swizzle/src/pch.h
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <tchar.h>
+#include <string.h>
 
 #define WIN32_LEAN_AND_MEAN     // Exclude rarely-used stuff from Windows headers
 #include <windows.h>
--- swizzle/src/swizzle.c
+++ swizzle/src/swizzle.c
@@ -5,6 +5,13 @@
 
 #define PLUGIN_API __declspec(dllexport)
 
+#define STRBUF_SIZE 4096
+#define ARGBUF_SIZE 1024
+#define MAX_FUNCSTR_SIZE 32000   // leave a few hundred charcters to spare
+
+static WCHAR *strbuf;
+static WCHAR *argbuf;
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved )
 {
     switch (ul_reason_for_call)
@@ -35,14 +42,36 @@ PLUGIN_API LPPLUGININFO WINAPI GetPluginInfo(HMODULE hModule) {
 }
 
 PLUGIN_API BOOL WINAPI InitializePlugin(void) {
+    strbuf = AllocMem(sizeof(WCHAR) * STRBUF_SIZE);
+    argbuf = AllocMem(sizeof(WCHAR) * ARGBUF_SIZE);
     return 0;
 }
 
 PLUGIN_API BOOL WINAPI ShutdownPlugin(BOOL bEndProcess) {
+    FreeMem(argbuf);
+    FreeMem(strbuf);
     return 0;
 }
 
+#define SAPPEND(sprint_func) \
+    do { \
+        n = sprint_func; \
+        if (n == -1) { return 1; } else { idx += n; remaining -= n; } \
+    } while (0)
+
 PLUGIN_API INT WINAPI f_swizzle(LPTSTR lpszString) {
-    Sprintf(lpszString, L"Swizzle!");
+    int n, idx = 0, remaining = STRBUF_SIZE - 1;
+    SAPPEND(swprintf_s(strbuf + idx, remaining, L"Parameter string: <<%s>>\n", lpszString));
+    for (int i = 0; i < 20; i++) {
+        if (NthArgument(lpszString, i, argbuf, NULL) != NULL) {
+            StripEnclosingQuotes(argbuf);
+            EscapeLine(argbuf);
+            SAPPEND(swprintf_s(strbuf + idx, remaining,  L"Arg %d <<%s>>\n", i + 1, argbuf));
+        } else {
+            break;
+        }
+    }
+    Printf(L"%s", strbuf);
+    wcscpy_s(lpszString, MAX_FUNCSTR_SIZE, L"Swizzle!");
     return 0;
 }
