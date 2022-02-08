dceadf44e00f698b5a45db7f72f8d4407b1d9c98
Jesse Pavel <jpavel@alum.mit.edu>
2022-02-08 15:54:58 -0500

    Remove unused tcchashmap.h

--- hashmap/src/tcchashmap.h
+++
@@ -1,9 +0,0 @@
-#define PLUGIN_API __declspec(dllexport)
-
-#define STRBUF_SIZE 4096
-#define ARGBUF_SIZE 1024
-#define MAX_FUNCSTR_SIZE 32000   // leave a few hundred charcters to spare
-#define MAX_PARAMS 20
-
-extern WCHAR *strbuf;
-extern WCHAR *argbuf;
