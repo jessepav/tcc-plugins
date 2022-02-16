507b37612e9a1635d6a3e0bd106db5cf72f8339c
Jesse Pavel <jpavel@alum.mit.edu>
2022-02-16 12:22:17 -0500

    hashnew and hashfree are working (?)

--- hashmap/src/tcchashmap.c
+++ hashmap/src/tcchashmap.c
@@ -9,14 +9,6 @@
 
 #define PLUGIN_API __declspec(dllexport)
 
-#define STRBUF_SIZE 4096
-#define ARGBUF_SIZE 1024
-#define MAX_FUNCSTR_SIZE 32000   // leave a few hundred charcters to spare
-#define MAX_PARAMS 20
-
-WCHAR *strbuf;
-WCHAR *argbuf;
-
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved )
 {
     switch (ul_reason_for_call)
@@ -47,14 +39,10 @@ PLUGIN_API LPPLUGININFO WINAPI GetPluginInfo(HMODULE hModule) {
 }
 
 PLUGIN_API BOOL WINAPI InitializePlugin(void) {
-    strbuf = AllocMem(sizeof(WCHAR) * STRBUF_SIZE);
-    argbuf = AllocMem(sizeof(WCHAR) * ARGBUF_SIZE);
     return 0;
 }
 
 PLUGIN_API BOOL WINAPI ShutdownPlugin(BOOL bEndProcess) {
-    FreeMem(argbuf);
-    FreeMem(strbuf);
     return 0;
 }
 
@@ -64,6 +52,16 @@ PLUGIN_API BOOL WINAPI ShutdownPlugin(BOOL bEndProcess) {
 
 #define MAX_DELIMITER_LENGTH 8
 #define DEFAULT_DELIMITER L"/"
+
+#define DEBUG 1
+
+#if DEBUG
+    #define DEBUG_PRINTF(format, ...) fwprintf(stderr, format, __VA_ARGS__)
+    #define DEBUG_PRINT(msg) fputws(msg, stderr)
+#else
+    #define DEBUG_PRINTF
+    #define DEBUG_PRINT
+#endif
 
 struct map {
     struct hashmap *hashmap;
@@ -113,41 +111,22 @@ PLUGIN_API INT WINAPI f_hashnew(LPTSTR paramStr) {
     // It may be the case, if the length of delimiter >= MAX_DELIMITER_LENGTH,
     // that a NULL is not appended, and so we terminate it manually.
     map->delimiter[MAX_DELIMITER_LENGTH] = L'\0';
+    DEBUG_PRINTF(L"Allocated a map at address 0x%p with delimiter \"%s\"\n", map, map->delimiter);
     getHandle(map, paramStr);
     return 0;
 }
 
 PLUGIN_API INT WINAPI f_hashfree(LPTSTR paramStr) {
     struct map *map = parseHandle(paramStr);
-    Printf(L"Freeing the map at address %p", map);
+    DEBUG_PRINTF(L"Freeing the map at address 0x%p\n", map);
+    paramStr[0] = L'\0';  // return an empty string
     hashmap_free(map->hashmap);
+    _set_errno(0);
     free(map);
-    return 0;
-}
-
-/* ================== Unused =================== */
-
-PLUGIN_API INT WINAPI f_hashmap(LPTSTR lpszString) {
-    UINT idx = 0, nParamIdx;
-    UINT nParams = 0;
-    idx += swprintf_s(strbuf + idx, STRBUF_SIZE - idx, L"Parameter string: <<%s>>\n", lpszString);
-    idx += swprintf_s(strbuf + idx, STRBUF_SIZE - idx, L"Number of parameters: ");
-    nParamIdx = idx;
-    idx += 3;  // reserve space to backpatch nParams (2 digits + newline)
-    for (int i = 0; i < MAX_PARAMS; i++) {
-        if (NthArgument(lpszString, i, argbuf, NULL) != NULL) {
-            StripEnclosingQuotes(argbuf);
-            EscapeLine(argbuf);
-            idx += swprintf_s(strbuf + idx, STRBUF_SIZE - idx,  L"Arg %d: <<%s>>\n", i + 1, argbuf);
-            nParams++;
-        } else {
-            break;
-        }
+    if (errno != 0) {
+        _wperror(L"f_hashfree");
+        return -1;
+    } else {
+        return 0;
     }
-    swprintf(argbuf, 4, L"%-2d\n", nParams);  // we use 4 because of the NULL terminator
-    wmemcpy(strbuf + nParamIdx, argbuf, 3);
-    strbuf[idx] = L'\0'; // if nParams == 0, the string might not be NULL-terminated
-    fputws(strbuf, stdout);
-    wcscpy_s(lpszString, MAX_FUNCSTR_SIZE, L"Swizzle!");
-    return 0;
 }
