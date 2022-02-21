4d804ed882704972fadaeead5fb755d913fb7d6d
Jesse Pavel <jpavel@alum.mit.edu>
2022-02-21 18:17:48 -0500

    Remove build.bat

--- hashmap/build.bat
+++
@@ -1,3 +0,0 @@
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -S . -B build\cmake-build ^
-      -DTCCHM_DEBUG=0 -DUSE_PCH=1 -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDLL
-cmake --build build\cmake-build
